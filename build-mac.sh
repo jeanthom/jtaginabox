39158c3015b9eba9f48a098c58b9d2802e08eba8
Jean THOMAS <git0@pub.jeanthomas.me>
2018-12-16 22:31:30 +0100

    Moving Info.plist ro res/

--- build-mac.sh
+++ build-mac.sh
@@ -4,6 +4,6 @@ mkdir -p JTAGinabox.app
 mkdir -p JTAGinabox.app/Contents/MacOS
 cp jtaginabox JTAGinabox.app/Contents/MacOS/
 cp libstlinkloader/libstlinkloader.dylib JTAGinabox.app/Contents/MacOS/
-cp Info.plist JTAGinabox.app/Contents/Info.plist
+cp res/Info.plist JTAGinabox.app/Contents/Info.plist
 install_name_tool -id "@executable_path/libstlinkloader.dylib" JTAGinabox.app/Contents/MacOS/libstlinkloader.dylib
 install_name_tool -change libstlinkloader.dylib "@executable_path/libstlinkloader.dylib" JTAGinabox.app/Contents/MacOS/jtaginabox
